e0fc54d0a913acd203164a123774cee935c37241
Alaa Nassar <alaanassar1211@gmail.com>
2023-10-15 18:15:29 +0300

    project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopbarComponent } from './layouts/topbar/topbar.component';
+import { SidebarComponent } from './layouts/sidebar/sidebar.component';
+import { VerticalComponent } from './layouts/vertical/vertical.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopbarComponent,
+    SidebarComponent,
+    VerticalComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
